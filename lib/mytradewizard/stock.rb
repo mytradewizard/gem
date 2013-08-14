a2af280a0f591bc7ce24d7f065f547f84186cc89
Zachary Skolnik <zskolnik@gmail.com>
2013-08-14 03:11:29 -0400

    added required parameter currency to IB::Contract, added readers for symbol & contract

--- lib/mytradewizard/stock.rb
+++ lib/mytradewizard/stock.rb
@@ -3,8 +3,12 @@ require 'ib'
 module MyTradeWizard
   class Stock
 
+    attr_reader :symbol
+    attr_reader :contract
+
     def initialize(symbol)
-      IB::Contract.new(:sec_type => :stock, :symbol => symbol)
+      @symbol = symbol
+      @contract = IB::Contract.new(:sec_type => :stock, :symbol => symbol, :currency => "USD")
     end
 
   end
