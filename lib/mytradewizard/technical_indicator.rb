c31c33c12429da158c74862af86de7e206178d37
Zachary Skolnik <zskolnik@gmail.com>
2013-08-14 03:19:01 -0400

    converted SMA from an instance method to a module method

--- lib/mytradewizard/technical_indicator.rb
+++ lib/mytradewizard/technical_indicator.rb
@@ -1,15 +1,17 @@
 module MyTradeWizard
   module TechnicalIndicator
+    class << self
 
-    def SMA(bars)
-      x = 0
-      n = 0
-      bars.each do |bar|
-        x += bar.close
-        n += 1
+      def SMA(bars)
+        x = 0
+        n = 0
+        bars.each do |bar|
+          x += bar.close
+          n += 1
+        end
+        return x / n
       end
-      return x / n
-    end
 
+    end
   end
 end
